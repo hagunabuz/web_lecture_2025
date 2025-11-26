53156c0783755ffd940a1aaa8275c1d22aed0668
장인영 <camcamicami@gmail.com>
2025-11-26 10:57:04 +0900

    ...

--- day14/index5.js
+++ day14/index5.js
@@ -1,13 +1,11 @@
 export default async function main() {
     console.log('start App')  
 
-
     const mainMenu = document.querySelector('#main-menu')
-    // const newGame = document.querySelector('')
     const creditScreen = document.querySelector('#credit-screen')
+    const optionScreen = document.querySelector('#option-screen')
     let currentScreen = "mainMenu"
 
-    
     let currentIndex = 0;
     const menuitems = document.querySelectorAll(".menu-item");
 
@@ -19,18 +17,22 @@ export default async function main() {
     window.addEventListener("keydown", (e) => {
         menuitems[currentIndex].classList.remove('select')
 
+        // =========================
+        // MAIN MENU
+        // =========================
         if(currentScreen == "mainMenu") {
 
             console.log(e.key)
+
             if(e.key == "ArrowUp") {
                 currentIndex--
                 if(currentIndex < 0) {
-                    currentIndex = menuitem_count-1
+                    currentIndex = menuitem_count - 1
                 }
             }
             else if(e.key == "ArrowDown") {
                 currentIndex++
-                currentIndex %=menuitem_count
+                currentIndex %= menuitem_count
             }
             else if(e.key == "Enter") {
                 console.log(menuitems[currentIndex].dataset.action)
@@ -41,30 +43,45 @@ export default async function main() {
                     creditScreen.classList.remove('hide')
                     currentScreen = "creditScreen"
                 }
+
+                else if(select_action == 'option') {
+                    mainMenu.classList.add('hide')
+                    optionScreen.classList.remove('hide')
+                    currentScreen = "optionScreen"
+                }
             }
 
             console.log(currentIndex)
             menuitems[currentIndex].classList.add('select')
-            
-
         }
+
+        // =========================
+        // CREDIT SCREEN
+        // =========================
         else if(currentScreen == "creditScreen") {
 
             console.log('credit screen')
 
             if(e.key == "Enter") {
-                
                 creditScreen.classList.add('hide')
                 mainMenu.classList.remove('hide')
                 currentScreen = "mainMenu"
             }
         }
 
-        
+        // =========================
+        // OPTION SCREEN
+        // =========================
+        else if(currentScreen == "optionScreen") {
+
+            console.log('option screen')
+
+            if(e.key == "Enter") {
+                optionScreen.classList.add('hide')
+                mainMenu.classList.remove('hide')
+                currentScreen = "mainMenu"
+            }
+        }
 
     })
-
-    
-
-
 }
